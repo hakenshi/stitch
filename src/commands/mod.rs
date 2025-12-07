957c4cef1706c3becdaa2d3ae9554f1dd37f2c02
hakenshi <fkdias025@gmail.com>
2025-12-07 08:49:09 -0300

    feat(commands): add CommandGenerator trait with default execute implementation

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,10 +1,2 @@
-mod make_component;
-
-pub struct MakeComponentOptions {
-    pub name: String,
-    pub with_children: bool,
-    pub with_props: bool,
-    pub with_styles: bool,
-}
-
-pub use make_component::execute as make_component;
+pub mod make_component;
+pub mod generator;
